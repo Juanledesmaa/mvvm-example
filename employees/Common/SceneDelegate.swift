04e3f60ced3095052b3f82aaf541107794cf1e04
Juan Ledesma <jlmanuel8540@gmail.com>
2022-10-24 20:48:22 -0400

    remove methods from sceneDelegate

--- employees/Common/SceneDelegate.swift
+++ employees/Common/SceneDelegate.swift
@@ -13,22 +13,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        if let windowScene = scene as? UIWindowScene {
-            self.window = UIWindow(windowScene: windowScene)
-            
-            if let window = self.window {
-                let router = MainRouter(window: window)
-                router.presentMainViewController()
-            }
-            
-//            let viewModel = EmployeesViewModel()
-//            let viewController = EmployeesViewController(viewModel: viewModel)
-//            self.window!.rootViewController = viewController
-//            self.window!.makeKeyAndVisible()
-        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
